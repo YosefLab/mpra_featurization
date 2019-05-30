fc230039b221bd7582e4636c95b39f712dbf2d90
Zhongxia Yan <zxyan@berkeley.edu>
2019-05-30 14:52:05 -0700

    added anonymize_tf to list for fasta

--- example_fasta/featurization_example.py
+++ example_fasta/featurization_example.py
@@ -7,6 +7,6 @@ example_dir = os.path.dirname(os.path.abspath(__file__))
 
 from featurizer import featurizer
 
-Features = ['deepsea', 'deepbind', '5mer', 'encode_matrix', 'fimo_summary', 'polyA_polyT_GC', 'dna_shape']
+Features = ['deepsea', 'deepbind', '5mer', 'encode_matrix', 'fimo_summary', 'polyA_polyT_GC', 'dna_shape', 'anonymize_tf']
 
-featurizer.generate_features(Features, example_dir + '/', max_num_threads=10)
+featurizer.generate_features(Features, example_dir + '/', max_num_threads=10, cell_type='hepg2')
